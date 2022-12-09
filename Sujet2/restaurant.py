60694d7d552a1534d6502a2864b77c68ee2d98d2
MathisGirardin <mathis.girardin@cpe.fr>
2022-12-09 17:24:17 +0100

    restaurant almost over

--- Sujet2/restaurant.py
+++ Sujet2/restaurant.py
@@ -1,11 +1,11 @@
 # Restaurant
 # Version de Girardin Mathis et Thomas Alexis
 
-from multiprocessing import Pipe
+from multiprocessing import Pipe, SimpleQueue
 from threading import Lock
 
 import multiprocessing as mp
-import os, time, math, random, sys, ctypes   
+import os, time, math, random, sys, ctypes
 import platform
 
 # Quelques codes d'échappement (tous ne sont pas utilisés)
@@ -27,7 +27,6 @@ NORMAL = "\x1B[0m"                  #  Normal
 BOLD = "\x1B[1m"                    #  Gras
 UNDERLINE = "\x1B[4m"               #  Souligné
 
-
 def effacer_ecran() : print(CLEARSCR,end = '')
 def erase_line_from_beg_to_curs() : print("\033[1K",end='')
 def erase_line() : print("\033[K",end='')
@@ -39,78 +38,94 @@ def en_couleur(Coul) : print(Coul,end='')
  
 NB_SERVEUR = 5
 WORKTIME = 1
+TIME_TO_COOK = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
+
+traitementCommandeEnCours = SimpleQueue()
 
 #----------------------------------------------------------------
-def serveur(TableauCommande : list, mutexCommande : Lock, serveurPipe):
+def serveur(idServeur, TableauCommande : SimpleQueue, serveurToMajordome):
     while True :
-        mutexCommande.acquire()
-        if TableauCommande.__len__() > 0:
+        if not TableauCommande.empty():
+            traitementCommandeEnCours.put(True)
             #Prise en charge de la commande
-            commande = TableauCommande.pop()
-            mutexCommande.release()
+            commande = str(TableauCommande.get())
+
+            serveurToMajordome.send(idServeur + commande + "R")
 
             #Transfert à la cuisine
-            print("La commande " + commande + " est en cuisine.")
-            serveurPipe.send("La commande " + commande + " est en cuisine.")
-            time.sleep(5)
+            indTTS = ord(commande[commande.index(',') + 1 : commande.index(')')]) - 65
+            time.sleep(TIME_TO_COOK[indTTS])
 
-            #Le majordome
-            serveurPipe.send("La commande " + commande + " est prête.")
-            print("La commande " + commande + " est prête.")
-        else:
-            mutexCommande.release()
+            serveurToMajordome.send(idServeur + commande + "P")
+            traitementCommandeEnCours.get()
 
-def clients(TableauCommande : list, mutexCommande : Lock):
+def clients(TableauCommande : SimpleQueue):
     while True :
-        mutexCommande.acquire()
-        TableauCommande.append("({},{})".format(random.randint(3,10), 65 + random.randint(0,25)))
-        mutexCommande.release()
-        print("Une commande a été ajouté au tableau")
+        TableauCommande.put("({},{})".format(random.randint(1,10), chr(65 + random.randint(0,25))))
         time.sleep(random.randint(3,10))
 
-def majordome(majordomePipe):
+def majordome(majordomeRecv):
     while True :
-        text = majordomePipe.recv()
+        data = str(majordomeRecv.recv())
+        line = int(data[0 : data.index("(")])
+        text = "Le serveur " + str(line) + " "
+        if data[data.index(")") + 1] == "R":
+            text = text + "s'occupe de la commande "
+        elif data[data.index(")") + 1] == "P":
+            text = text + "a servi la commande "
+        text = text + data[data.index("(") : data.index(")") + 1]
+        move_to(line, 0)
+        erase_line()
         print(text)
 
-def ouverture_restaurant(ProcessList : list, TableauCommande : list, mutexCommande):
-    majordomePipe, serveurPipe = Pipe()
+def ouverture_restaurant(ProcessList : list, TableauCommande : SimpleQueue):
+    majordomeRecv, serveurToMajordome = Pipe()
 
     #Arrivée des employés
     #Majordome
-    process = mp.Process(target = majordome, args = (majordomePipe,))
+    process = mp.Process(target = majordome, args = (majordomeRecv,))
     process.start()
     ProcessList.append(process)
 
     #Esclaves
     for i in range(NB_SERVEUR):
-        process = mp.Process(target = serveur, args = (TableauCommande, mutexCommande, serveurPipe))
+        process = mp.Process(target = serveur, args = (str(i + 1), TableauCommande, serveurToMajordome))
         process.start()
         ProcessList.append(process)
 
-    print("Ouverture")
+    move_to(NB_SERVEUR + 3, 0)
+    erase_line()
+    print("Ouvert")
 
     #Arrivée des clients
-    process = mp.Process(target = clients, args = (TableauCommande, mutexCommande))
+    process = mp.Process(target = clients, args = (TableauCommande,))
     process.start()
     ProcessList.append(process)
 
-def fermeture_restaurant(ProcessList : list, TableauCommande : list, mutexCommande):
+def fermeture_restaurant(ProcessList : list, TableauCommande : SimpleQueue):
     #Fin des commandes
     ProcessList[ProcessList.__len__() - 1].terminate()
+    move_to(NB_SERVEUR + 3, 0)
+    erase_line()
     print("Arrêt des commandes")
+
     #Arrêt du service
-    mutexCommande.acquire()
-    while not TableauCommande.__len__() == 0:
-        mutexCommande.release()
-        mutexCommande.acquire()
+    while not TableauCommande.empty():
         continue
-    mutexCommande.release()
-    for i in range(1, NB_SERVEUR + 1):
+    while not traitementCommandeEnCours.empty():
+        print("n",end='')
+        time.sleep(1)
+        continue
+
+    print("OKKKKKKKKK")
+
+    #Départ du majordome et des serveurs
+    for i in range(0, NB_SERVEUR + 1):
         ProcessList[i].terminate()
-    #Départ du majordome
-    ProcessList[0].terminate()
-    print("Fermeture")
+
+    move_to(NB_SERVEUR + 3, 0)
+    erase_line()
+    print("Fermer")
 
 # ---------------------------------------------------
 # La partie principale :
@@ -119,9 +134,13 @@ if __name__ == "__main__" :
         mp.set_start_method('fork') # Nécessaire sous macos, OK pour Linux (voir le fichier des sujet
 
     ProcessList = []
-    TableauCommande = []
-    mutexCommande = Lock()
-    ouverture_restaurant(ProcessList, TableauCommande, mutexCommande)
-    for i in range(WORKTIME):
-        time.sleep(10)
-    fermeture_restaurant(ProcessList, TableauCommande, mutexCommande)
+    TableauCommande = SimpleQueue()
+
+    while True :
+        ouverture_restaurant(ProcessList, TableauCommande)
+        for i in range(WORKTIME):
+            time.sleep(10)
+        fermeture_restaurant(ProcessList, TableauCommande)
+        for i in range(WORKTIME):
+            time.sleep(10)
+
